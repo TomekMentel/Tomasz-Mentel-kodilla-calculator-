a8566b3d82a70e8bd1a2f3e60502cecf9ed95b24
unknown <mentell@o2.pl>
2019-04-07 20:43:21 +0200

    first commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,70 +1,34 @@
 package com.kodilla;
 
-
 public class Calculator {
 
-
     int x;
-
     int y;
-
 
     public static void main(String[] args) {
 
         System.out.println("Liczby 5 i 8 ");
 
         Calculator calculator = new Calculator();
-
         double suma = calculator.suma(5, 8);
-
-        System.out.println("wynik Sumowanie:" + Suma);
-
+        System.out.println("wynik Sumowanie:" + suma);
         double subtraction = calculator.sub(5, 8);
-
-        System.out.println("Wynik odejmowania:" + Subtraction);
-
+        System.out.println("Wynik odejmowania:" + subtraction);
         double multiplication = calculator.multi(5, 8);
-
-        System.out.println("Wynik Mnozenie:" + Multiplication);
-
+        System.out.println("Wynik Mnozenie:" + multiplication);
         double division = calculator.divi(5, 8);
-
-        System.out.println("Wynik dzielenia:" + Division);
-
+        System.out.println("Wynik dzielenia:" + division);
     }
-
-
     int suma(int x, int y) {
-
         return x + y;
-
-
     }
-
-
     int multi(int x, int y) {
-
-
         return x * y;
-
-
     }
-
-
     int sub(int x, int y) {
-
         return x - y;
-
-
     }
-
-
     double divi(double x, double y) {
-
-
         return x / y;
-
-
     }
-
 }
